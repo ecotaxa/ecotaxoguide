59176c069f2b52f46a97e450a6086f1aaa0054a5
grololo06 <laurent.salinas@laposte.net>
2022-04-11 06:41:17 +0200

    Simplify segment path.

--- taxo_card/interfaces/scratch.py
+++ taxo_card/interfaces/scratch.py
@@ -6,4 +6,12 @@
 - Rajouter une view: interface + simple "Add "+view name dans un DropDown
 """
 
-
+# SVG libs HTML:
+#
+# https://github.com/DmitryBaranovskiy/raphael
+# Author of which did:
+# http://snapsvg.io/
+#
+# https://svgjs.dev/docs/3.1
+#
+# https://vectorjs.org/ with edition elements
